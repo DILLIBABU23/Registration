24015065424a3a419d957dc4da3c690f9d3284f2
DILLIBABU23 <102577264+DILLIBABU23@users.noreply.github.com>
2024-11-19 14:38:04 +0530

    RegistrationApplication.java

--- Backend/RegistrationApplication.java
+++ Backend/RegistrationApplication.java
@@ -1,13 +1,13 @@
-package com.DHR.simpleWebApp;
+package com.tap.RegistrationApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleWebAppApplication {
+public class RegistrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleWebAppApplication.class, args);
+		SpringApplication.run(RegistrationApplication.class, args);
 	}
 
 }
