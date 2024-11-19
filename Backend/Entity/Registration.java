2093ecb057396209390b97a15ad33390b60b3ede
DILLIBABU23 <102577264+DILLIBABU23@users.noreply.github.com>
2024-11-19 14:45:24 +0530

    Create Registration.java

---
+++ Backend/Entity/Registration.java
@@ -0,0 +1,29 @@
+package com.tap.Registration.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Email is invalid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Past(message = "Date of Birth must be in the past")
+    private LocalDate dateOfBirth;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdAt = LocalDate.now();
+}
