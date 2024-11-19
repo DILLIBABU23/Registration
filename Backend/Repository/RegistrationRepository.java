360f9af5c08144231e84017457c7d2d143377f47
DILLIBABU23 <102577264+DILLIBABU23@users.noreply.github.com>
2024-11-19 14:51:11 +0530

    Create RegistrationRepository.java

---
+++ Backend/Repository/RegistrationRepository.java
@@ -0,0 +1,10 @@
+package com.tap.Registration.repository;
+
+import com.tap.Registration.entity.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+  
+}
