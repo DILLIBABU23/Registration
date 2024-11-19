78fbaa3190924988a24f786f05c8fb75ba69eab7
DILLIBABU23 <102577264+DILLIBABU23@users.noreply.github.com>
2024-11-19 14:55:20 +0530

    RegistrationRepository.java

--- Backend/Repository/RegistrationRepository.java
+++ Backend/Repository/RegistrationRepository.java
@@ -1,6 +1,6 @@
 package com.tap.Registration.repository;
 
-import com.tap.Registration.entity.Registration;
+import com.tap.Registration.Entity.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
